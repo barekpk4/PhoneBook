f9247af53be0888f02a8612551501913717fa285
User <User@DESKTOP-P7K57AH>
2022-10-26 11:45:24 +0600

    Add project files.

---
+++ WindowsFormsApp55/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace WindowsFormsApp55
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.txtLF = new System.Windows.Forms.TextBox();
+            this.txtMN = new System.Windows.Forms.TextBox();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(274, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PhoneBook";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mobile Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(84, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(84, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Relations";
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(277, 119);
+            this.txtFN.Name = "txtFN";
+            this.txtFN.Size = new System.Drawing.Size(226, 22);
+            this.txtFN.TabIndex = 6;
+            // 
+            // txtLF
+            // 
+            this.txtLF.Location = new System.Drawing.Point(277, 164);
+            this.txtLF.Name = "txtLF";
+            this.txtLF.Size = new System.Drawing.Size(226, 22);
+            this.txtLF.TabIndex = 7;
+            // 
+            // txtMN
+            // 
+            this.txtMN.Location = new System.Drawing.Point(277, 217);
+            this.txtMN.Name = "txtMN";
+            this.txtMN.Size = new System.Drawing.Size(226, 22);
+            this.txtMN.TabIndex = 8;
+            // 
+            // txtE
+            // 
+            this.txtE.Location = new System.Drawing.Point(277, 272);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(226, 22);
+            this.txtE.TabIndex = 9;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(277, 336);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(226, 22);
+            this.txtR.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(84, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 30);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "City";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dhaka",
+            "Rajshahi",
+            "Natore",
+            "Jessore",
+            "Pabna"});
+            this.comboBox1.Location = new System.Drawing.Point(277, 397);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(282, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(428, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(574, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 34);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(737, 456);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 34);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(885, 456);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 34);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Find";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(525, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(810, 301);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1347, 526);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.txtMN);
+            this.Controls.Add(this.txtLF);
+            this.Controls.Add(this.txtFN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFN;
+        private System.Windows.Forms.TextBox txtLF;
+        private System.Windows.Forms.TextBox txtMN;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp55/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp55
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-P7K57AH\SQLEXPRESS;Initial Catalog=crud;Integrated Security=true;");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname = txtFN.Text;
+            string lname = txtLF.Text;
+            int mobile = Convert.ToInt32(txtMN.Text);
+            string email = txtE.Text;
+            string relation = txtR.Text;
+            string city = (comboBox1.SelectedItem).ToString();
+
+
+
+            SqlCommand cmd = new SqlCommand("insert into phone values('" + fname + "','" + lname + "'," + mobile + ",'" + email + "','" + relation + "','" + city + "')", con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ClearAll();
+            LoadData();
+
+        }
+        private void LoadData()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select*from phone", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+
+        private void ClearAll()
+        {
+            txtFN.Clear();
+            txtLF.Clear();
+            txtMN.Focus();
+            txtE.Clear();
+            txtR.Clear(); //this is a little mistake//
+                          // comboBox1.
+            comboBox1.SelectedIndex = -1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fname = txtFN.Text;
+            string lname = txtLF.Text;
+            int mobile = Convert.ToInt32(txtMN.Text);
+            string email = txtE.Text;
+            string relation = txtR.Text;
+            string city = (comboBox1.SelectedItem).ToString();
+
+
+            SqlCommand cmd = new SqlCommand("update phone set fname='" + fname + "',lname='" + lname + "',email='" + email + "',relation='" + relation + "',city='" + city + "' where mobile='" + mobile + "'", con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ClearAll();
+            LoadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int mobile = Convert.ToInt32(txtMN.Text);
+
+            SqlDataAdapter sda = new SqlDataAdapter("select * from phone where mobile ='" + mobile + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                txtFN.Text = dt.Rows[0][0].ToString();
+                txtLF.Text = dt.Rows[0][1].ToString();
+
+                txtE.Text = dt.Rows[0][3].ToString();
+                txtR.Text = dt.Rows[0][4].ToString();
+                comboBox1.Text = dt.Rows[0][5].ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int mobile = Convert.ToInt32(txtMN.Text);
+            SqlCommand cmd = new SqlCommand("DELETE   FROM PHONE WHERE mobile='" + mobile + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            LoadData();
+        }
+    }
+}
